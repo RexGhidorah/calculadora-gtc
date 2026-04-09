813ac10b27dd3338b93abaf59ada787182efee98
RexGhidorah <168611490+RexGhidorah@users.noreply.github.com>
2026-04-09 11:07:05 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   server: {
-    allowedHosts: [
-      'computation-bags-stripes-compaq.trycloudflare.com'
-    ]
+    // Esto permite cualquier host en el servidor de desarrollo
+    allowedHosts: true 
   }
 })
